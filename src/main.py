e6b4144d5ed689826d9683f0e3f89c631c5a438f
Richard Michael Coo <richard.michael.coo@gmail.com>
2025-10-08 22:46:56 +0800

    ♻️ refactor: make main.py declarative

--- src/main.py
+++ src/main.py
@@ -1,39 +1,33 @@
 import flet as ft
 
-def main(page: ft.Page):
-    page.title = "Flet counter example"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    
-    # Add AppBar
-    page.appbar = ft.AppBar(
-        title=ft.Text("Counter App"),
-        bgcolor=ft.Colors.GREEN,
-        center_title=True,
-        actions=[
-            ft.IconButton(ft.Icons.SETTINGS),
-            ft.IconButton(ft.Icons.INFO),
+
+@ft.component
+def App():
+    count, set_count = ft.use_state(0)
+
+    return ft.View(
+        appbar=ft.AppBar(
+            title=ft.Text("Counter App"),
+            bgcolor=ft.Colors.GREEN,
+            center_title=True,
+            actions=[
+                ft.IconButton(ft.Icons.SETTINGS),
+                ft.IconButton(ft.Icons.INFO),
+            ],
+        ),
+        floating_action_button=ft.FloatingActionButton(
+            icon=ft.Icons.ADD, on_click=lambda: set_count(count + 1)
+        ),
+        controls=[
+            ft.SafeArea(
+                ft.Container(
+                    ft.Text(value=f"{count}", size=50),
+                    alignment=ft.Alignment.CENTER,
+                ),
+                expand=True,
+            )
         ],
     )
 
-    input = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
 
-    def minus_click(e):
-        input.value = str(int(input.value) - 1)
-        page.update()
-
-    def plus_click(e):
-        input.value = str(int(input.value) + 1)
-        page.update()
-
-    page.add(
-        ft.Row(
-            alignment=ft.MainAxisAlignment.CENTER,
-            controls=[
-                ft.IconButton(ft.Icons.REMOVE, on_click=minus_click),
-                input,
-                ft.IconButton(ft.Icons.ADD, on_click=plus_click),
-            ],
-        )
-    )
-
-ft.run(main)
+ft.run(lambda page: page.render_views(App))
